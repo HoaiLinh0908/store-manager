f2525029452286ef444f557aa1df144dcde81b64
Linh Do <hoailinh9966@gmail.com>
2024-04-21 15:09:38 +0700

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
+import { StoreModule } from './store/store.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, PrismaModule, UserModule, StoreModule],
 })
 export class AppModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
 
 @Injectable()
 export class AuthService {
-  signup() {
-    return { msg: 'Sign up here!' };
+  constructor(private prismaService: PrismaService) {}
+  async signup(dto: AuthDto) {
+    //generate password
+    const hash = await argon.hash(dto.password);
+
+    //save the new user
+    const user = await this.prismaService.user.create({
+      data: {
+        email: dto.email,
+        hash,
+        username: dto.email,
+      },
+    });
+
+    //return the saved user
+    return user;
   }
 
   signin() {
